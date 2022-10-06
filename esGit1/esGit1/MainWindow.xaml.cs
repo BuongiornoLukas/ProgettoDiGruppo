539e431dc7d1828c3155cd37cf5809d544dd2c62
Lukas Magalotti <LUKAS.MAGALOTTI@local.ittcesena.it>
2022-10-06 09:05:22 +0200

    versione base

--- esGit1/esGit1/MainWindow.xaml.cs
+++ esGit1/esGit1/MainWindow.xaml.cs
@@ -13,16 +13,46 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace esGit1
+namespace Attivita_1GIT
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        double[] v = new double[10];
         public MainWindow()
         {
             InitializeComponent();
+
+            Random r = new Random();
+
+            for (int i = 0; i < 10; i++)
+                v[i] = r.Next(0, 100);
+        }
+        // button per il calcolo della media
+        private void btnMedia_Click(object sender, RoutedEventArgs e)
+        {
+            double somma = 0;
+
+            for (int i = 0; i < 10; i++)
+                somma += v[i];
+
+            lblMedia.Content = somma / 10;
+        }
+
+        //button per il calcolo del minimo
+        private void btnMin_Click(object sender, RoutedEventArgs e)
+        {
+            double min = v[0];
+
+            for (int i = 1; i < 10; i++)
+            {
+                if (v[i] < min)
+                    min = v[i];
+            }
+
+            lblMin.Content = min;
         }
     }
 }
